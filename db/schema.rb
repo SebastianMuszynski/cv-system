b2dfdfe781090555395658593e5cee026269cadd
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2015-02-24 17:02:02 +0000

    Adedd photo, first_name and last_name to User.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 20150205143825) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "photo"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "K123456@kingston.ac.uk", password: "123123123", photo: "", first_name: "Sebastian", last_name: "Muszynski")
