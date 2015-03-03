1504404d8e4e064e2f4b189c4661864c706a2939
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2015-03-03 16:39:15 +0000

    Added My Account to Staff member.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def main
-    render layout: "homepage"
+    render layout: "homepage" unless (user_signed_in? || staff_member_signed_in?)
   end
 end
