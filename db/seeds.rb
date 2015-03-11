06fe1d1137f4de3416561ea65d127d81ca4d4a78
Ace Shaulov <k1230640@kingston.ac.uk>
2015-03-11 12:32:03 +0000

    Added Users data to the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,14 @@
 #
 
 User.create(email: "K123456@kingston.ac.uk", password: "123123123", photo: "", first_name: "Sebastian", last_name: "Muszynski")
+User.create(email: "K395671@kingston.ac.uk", password: "Hell6666", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/gerrenlamson/128.jpg", first_name: "Abraham", last_name: "McLeod")
+User.create(email: "K193486@kingston.ac.uk", password: "Jingle12", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/idiot/128.jpg", first_name: "John", last_name: "Smith")
+User.create(email: "K432354@kingston.ac.uk", password: "KerrySpy", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg", first_name: "Pamela", last_name: "Henderson")
+User.create(email: "K735692@kingston.ac.uk", password: "Chirka1234", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/minipunk/128.jpg", first_name: "Craig", last_name: "Farber")
+User.create(email: "K174682@kingston.ac.uk", password: "GoatsAreCrazy", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/stylecampaign/128.jpg", first_name: "Jane", last_name: "McAllison")
+User.create(email: "K222987@kingston.ac.uk", password: "EBELLARp", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/annapickard/128.jpg", first_name: "Emily", last_name: "Bellar")
+User.create(email: "K876401@kingston.ac.uk", password: "RocknRoll", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/davitferreira/128.jpg", first_name: "Alice", last_name: "Booper")
+User.create(email: "K111116@kingston.ac.uk", password: "Chiseng1", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/5key/128.jpg", first_name: "Jet", last_name: "Li")
+User.create(email: "K243513@kingston.ac.uk", password: "SwimProf", photo: "https://s3.amazonaws.com/uifaces/faces/twitter/joostvanderree/128.jpg", first_name: "Kim", last_name: "Phillips")
+
 StaffMember.create(email: "K123456@kingston.ac.uk", password: "123123123", first_name: "Sebastian", last_name: "Muszynski")
