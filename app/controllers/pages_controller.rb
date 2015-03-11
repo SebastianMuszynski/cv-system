bd3bbd0044dd7697cfdd6d355c4883356a9f5791
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2015-03-11 23:26:23 +0000

    Fixed homepage routing.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def main
-    render layout: "homepage" unless (user_signed_in? || staff_member_signed_in?)
+    redirect_to admin_jobs_path and return if staff_member_signed_in?
+    redirect_to jobs_path       and return if user_signed_in?
+    render layout: "homepage"
   end
 end
