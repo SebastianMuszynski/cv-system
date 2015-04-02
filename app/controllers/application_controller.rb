914920856331ae84f5eb37919998d8e36c796e69
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2015-04-02 13:09:24 +0100

    Allowing user to sign up with custom first name and last name, and update those information.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |user|
+      user.permit(:email, :first_name, :last_name, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |user|
+      user.permit(:email, :first_name, :last_name, :password, :password_confirmation, :current_password)
+    end
+  end
 end
