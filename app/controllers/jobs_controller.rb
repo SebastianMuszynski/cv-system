52e47f355abf01b411098f8a8ee14df46fabcc95
Ace Shaulov <k1230640@kingston.ac.uk>
2015-05-01 12:02:53 +0000

    Highlighting currently selected category.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,17 +15,20 @@ class JobsController < ApplicationController
 
   def filter_by_category jobs
     return jobs if params[:category].nil? || params[:category] == "All categories"
+    @current_category = params[:category]
     jobs.where category: params[:category]
   end
 
   def filter_by_created_date jobs
-    return jobs unless params[:date].present? 
+    return jobs unless params[:date].present?
+    @current_date = params[:date]
     created_date_order = params[:date] == "Oldest" ? :asc : :desc
     jobs.reorder created_at: created_date_order
   end
 
   def filter_by_deadline jobs
     return jobs unless params[:deadline].present? 
+    @current_deadline = params[:deadline]
     if params[:deadline] == "Upcoming"
       jobs.reorder deadline: :asc
     elsif params[:deadline] == "Furthest"
